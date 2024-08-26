f042fa345a98db969ef0b84db5ebf9f08a35edc4
Hyeon9mak <jinha3507@gmail.com>
2024-08-27 00:53:28 +0900

    init project

---
+++ src/main/kotlin/com/example/randomdrawcard/RandomDrawCardApplication.kt
@@ -0,0 +1,11 @@
+package com.example.randomdrawcard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RandomDrawCardApplication
+
+fun main(args: Array<String>) {
+    runApplication<RandomDrawCardApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/randomdrawcard/RandomDrawCardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.randomdrawcard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RandomDrawCardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
